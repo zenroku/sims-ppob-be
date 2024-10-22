22dabf449659ca5eb08a0735f2aa80504e59843d
Zahir Fathurrahman <zahir.frahman@gmail.com>
2024-10-22 21:32:32 +0700

    initial commit

---
+++ src/main/java/co/id/zahir/nutech/simsppob/SimsppobApplication.java
@@ -0,0 +1,13 @@
+package co.id.zahir.nutech.simsppob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsppobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimsppobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/zahir/nutech/simsppob/SimsppobApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.zahir.nutech.simsppob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimsppobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
